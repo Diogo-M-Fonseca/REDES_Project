cdeac1c9965b38bd465cad350f19ae8d98146790
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2026-05-06 22:54:48 +0100

    created Deck script

---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    private Card[] cards = new Card[53];
+
+    private int Topcard;
+
+    public void initialize()
+    {
+        int index = 0;
+
+        foreach (Enum_Suit suit in System.Enum.GetValues(typeof(Enum_Suit)))
+        {
+            for (int value = 1; value <= index; value++)
+            {
+                //calculo do valor das cartas de blackjack
+                int blackjackValue = Mathf.Min(value, 10);
+                cards[index] = new Card(blackjackValue, suit);
+            }
+        }
+        Topcard = cards.Length - 1;
+    }
+
+
+}
