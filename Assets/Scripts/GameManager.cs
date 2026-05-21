0f12b11802ef86a14527ca9c3d6cc7d943f31561
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2026-05-21 15:39:41 +0100

    Gamemanager new try

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,19 @@
 using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
+using Unity.Netcode;
 
-public class GameManager : MonoBehaviour
+public class GameManager : NetworkBehaviour
 {
-    
+    private Deck deck;
+
+    private void Start()
+    {
+        if (IsServer)
+        {
+            deck = new Deck();
+            deck.Initialize();
+        }
+    }
+
 }
